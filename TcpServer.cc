ef062194cdd7499334edad38eb608942278d3368
jqivin-jdcloud <jqivin@163.com>
2025-05-27 22:54:36 +0800

    add TcpServer.h

--- TcpServer.cc
+++ TcpServer.cc
@@ -1,1 +1,38 @@
 #include "TcpServer.h"
+#include "Logger.h"
+
+static EventLoop* CheackLoopNotNull(EventLoop* loop)
+{
+    if(loop == nullptr)
+    {
+        LOG_FATAL("%s:%s:%d mainloop is nullptr \n", __FILE__, __FUNCTION__, __LINE__);
+    }
+    return loop;
+}
+
+TcpServer::TcpServer(EventLoop* loop,
+        const InetAddress &listenAddr,
+        const std::string &nameArg,
+        Option option = kNoReusePort)
+        : loop_(loop)
+        , name_(nameArg)
+        , ipPort_(listenAddr.toIpPort())
+        , acceptor_(new Acceptor(loop,listenAddr,option))
+        , threadPool_(new EventLoopThreadPool(loop,name_))
+        , connectionCallback_()
+        , messageCallback_()
+        , nextConnId_(1)
+        , started_(0)
+{
+    // 设置newConnecttion回调，当有新用户连接的时候，会执行这个回调
+}
+
+TcpServer::~TcpServer()
+{
+
+}
+
+void TcpServer::setThreadNums(int threadNums)
+{
+    
+}
--- TcpServer.h
+++ TcpServer.h
@@ -1,3 +1,73 @@
 #pragma once
 
+#include "EventLoop.h"
+#include "Acceptor.h"
+#include "InetAddress.h"
+#include "noncopyable.h"
+#include "EventLoopThreadPool.h"
+#include "Callbacks.h"
+#include "TcpConnection.h"
+#include "Buffer.h"
+
+#include <functional>
+#include <string>
+#include <memory>
+#include <atomic>
+#include <unordered_map>
+
+/**
+ * 用户使用muduo编写服务器程序
+ */ 
 // 对外的服务器编程使用的类
+class TcpServer
+{
+    using ThreadInitCallback = std::function<void(EventLoop*)>;
+
+    enum Option
+    {
+        kNoReusePort,
+        kReusePort,
+    };
+public:
+    TcpServer(EventLoop* loop,
+        const InetAddress &listenAddr,
+        const std::string &nameArg,
+        Option option = kNoReusePort);
+    ~TcpServer();
+
+    void setThreadNums(int threadNums);     // 设置底层subloop的个数
+
+    // 设置回调
+    void setThreadInitcallback(const ThreadInitCallback &cb) { threadInitCallback_ = cb; }
+    void setConnectionCallback(const ConnectionCallback &cb) { connectionCallback_ = cb; }
+    void setMessageCallback(const MessageCallback &cb) { messageCallback_ = cb; }
+    void setWriteCompleteCallback(const WriteCompleteCallback &cb) { writeCompleteCallback_ = cb; }
+
+    // 开启服务器监听
+    void start();
+
+private:
+    void newConnection(int sockfd, const InetAddress &peerAddr);
+    void removeConnection(const TcpConnectionPtr &conn);
+    void removeConnectionInLoop(const TcpConnectionPtr &conn);
+    
+    using ConnectionMap = std::unordered_map<std::string, TcpConnectionPtr>;
+    
+    EventLoop* loop_;       // baseLoop 用户定义的loop
+    std::string name_;      
+    std::string ipPort_;
+
+    std::unique_ptr<Acceptor> acceptor_;    //运行在mainloop，任务监听新用户的连接
+    std::shared_ptr<EventLoopThreadPool> threadPool_;   // one loop peer thread
+
+    ConnectionCallback connectionCallback_; // 有新连接时的回调
+    MessageCallback messageCallback_; // 有读写消息时的回调
+    WriteCompleteCallback writeCompleteCallback_; // 消息发送完成以后的回调
+
+    ThreadInitCallback threadInitCallback_; // loop线程初始化的回调
+
+    std::atomic<int> started_;
+    int nextConnId_;                // 用于分配subloop
+    ConnectionMap connections_;     // 保存所有的连接
+
+};
