78b06a7953e4d9ae6f3412edb643e52fd4130312
jqivin-jdcloud <jqivin@163.com>
2025-07-31 22:58:01 +0800

    add TcpServer

---
+++ TcpConnection.cc
@@ -0,0 +1,47 @@
+#include "TcpConnection.h"
+#include "Logger.h"
+#include "Socket.h"
+#include "Channel.h"
+#include "EventLoop.h"
+
+#include <functional>
+#include <errno.h>
+#include <sys/types.h>         
+#include <sys/socket.h>
+#include <strings.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <string>
+
+static EventLoop* CheckLoopNotNull(EventLoop* loop)
+{
+    if(loop == nullptr)
+    {
+        LOG_FATAL("%s:%s:%d TcpConnection Loop is null! \n", __FILE__, __FUNCTION__, __LINE__);
+    }
+
+    return loop;
+}
+
+TcpConnection::TcpConnection(EventLoop* loop,
+                const std::string& name,
+                int sockfd,
+                const InetAddress& localAddr,
+                const InetAddress& peerAddr)
+    :loop_(CheckLoopNotNull(loop))
+    , name_(name)
+    , state_(kConnecting)
+    , reading_(true)
+    , socket_(new Socket(sockfd))
+    , channel_(new Channel(loop,sockfd))
+    , localAddr_(localAddr)
+    , peerAddr_(peerAddr)
+    , highWaterMark_(64*1024*1024)  // 64M
+{
+
+}
+    
+TcpConnection::~TcpConnection()
+{
+
+}
--- TcpServer.cc
+++ TcpServer.cc
@@ -1,5 +1,9 @@
 #include "TcpServer.h"
 #include "Logger.h"
+#include "TcpConnection.h"
+
+#include <strings.h>
+#include <functional>
 
 static EventLoop* CheackLoopNotNull(EventLoop* loop)
 {
@@ -49,7 +53,65 @@ void TcpServer::setThreadNums(int threadNums)
     threadPool_->setThreadNum(threadNums);
 }
 
- void TcpServer::newConnection(int sockfd, const InetAddress &peerAddr)
- {
+// 有一个新的客户端的连接，acceptor会执行这个回调操作 在TcpServer的构造函数中设置了这个回调函数
+void TcpServer::newConnection(int sockfd, const InetAddress &peerAddr)
+{
+    //  轮询算法，选择一个subLoop，来管理channel
+    EventLoop* ioloop = threadPool_->getNextLoop();
 
- }
+    char buf[64] = {0};
+    snprintf(buf,sizeof buf,"-%s#%d",ipPort_.c_str(),nextConnId_);
+    ++nextConnId_;
+    std::string connName = name_ + buf;
+
+    LOG_INFO("TcpServer::newConnection [%s] - new connection [%s] from %s \n",
+        name_.c_str(), connName.c_str(), peerAddr.toIpPort().c_str());
+
+        // 通过sockfd获取其绑定的本机的ip地址和端口信息
+    sockaddr_in local;
+    ::bzero(&local,sizeof local);
+    socklen_t addrlen = sizeof local;
+    if(::getsockname(sockfd,(sockaddr*)&local,&addrlen) < 0)
+    {
+        LOG_ERROR("sockets::getLocalAddr");
+    }
+    InetAddress localAddr(local);
+
+    // 根据连接成功的sockfd，创建TcpConnection连接对象
+    TcpConnectionPtr conn(new TcpConnection(ioloop,
+                                connName,
+                                sockfd,
+                                localAddr,
+                                peerAddr));
+    connections_[connName] = conn;
+
+    // 给TcpConnection对象设置回调，这些都是用户设置给TcpServer的
+    conn->setConnectionCallback(connectionCallback_);
+    conn->setMessageCallback(messageCallback_);
+    conn->setWriteCompleteCallback(writeCompleteCallback_);
+
+    // 设置了如何关闭的回调 当coon->shutdown的时候调用
+    conn->setCloseCallback(std::bind(&TcpServer::removeConnection,this,std::placeholders::_1));
+
+    // 直接调用TcpConnection::connectEstablished
+    ioloop->runInLoop(std::bind(&TcpConnection::connectEstablished,conn));
+}
+
+void TcpServer::removeConnection(const TcpConnectionPtr &conn)
+{
+    loop_->runInLoop(
+        std::bind(&TcpServer::removeConnectionInLoop, this, conn)
+    );
+}
+
+void TcpServer::removeConnectionInLoop(const TcpConnectionPtr &conn)
+{
+    LOG_INFO("TcpServer::removeConnectionInLoop [%s] - connection %s\n", 
+        name_.c_str(), conn->name().c_str());
+    
+    connections_.erase(conn->name());
+    EventLoop* ioLoop = conn->getLoop();
+    ioLoop->queueInLoop(
+        std::bind(&TcpConnection::connectDestroyed,conn)
+    );
+}
